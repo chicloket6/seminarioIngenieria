b8824ba570a90bc6b52679f41e62901a97899ed7
unknown <raya9631@gmail.com>
2020-03-30 18:34:00 -0600

    tabla habitacion

---
+++ database/migrations/2020_03_31_002306_create_habitaciones_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHabitacionesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('habitaciones', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('numero');
+            $table->unsignedBigInteger('tipos_habitacion_id')->nullable();
+            $table->foreign('tipos_habitacion_id')->references('id')->on('tipos_habitacion');
+            $table->unsignedBigInteger('status_id')->nullable();
+            $table->foreign('status_id')->references('id')->on('status_habitacion');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('habitaciones');
+    }
+}
