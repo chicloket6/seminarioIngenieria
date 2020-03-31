cd9af00a6bc8d927e923470ae758b1af89304d2c
Rikupan <57078873+Rikupan@users.noreply.github.com>
2020-03-30 18:36:15 -0600

    tabla promociones

---
+++ database/migrations/2020_03_31_002259_create_promociones_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePromocionesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promociones', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('fecha_inicio');
+            $table->dateTime('fecha_final');
+            $table->integer('descuento');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promociones');
+    }
+}
