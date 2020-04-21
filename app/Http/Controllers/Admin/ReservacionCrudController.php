7f08e39966fe8f48d1e89b6f8c0bccb39f23c39f
chicloket6 <samarripa_17@hotmail.com>
2020-04-21 18:07:48 -0500

    search logic para buscar en todas las columnas en vista de reservaciones

--- app/Http/Controllers/Admin/ReservacionCrudController.php
+++ app/Http/Controllers/Admin/ReservacionCrudController.php
@@ -40,148 +40,175 @@ class ReservacionCrudController extends CrudController
 
     protected function setupListOperation()
     {
-        // TODO: remove setFromDb() and manually define Columns, maybe Filters
-        //$this->crud->setFromDb();
-        $this->crud->addColumn([
-            'name' => "fecha_entrada", // The db column name
-            'label' => "Fecha De Entrada", // Table column heading
-            'type' => "datetime",
-             // 'format' => 'l j F Y H:i:s', // use something else than the base.default_datetime_format config value
-         ]);
+      $this->crud->addColumn([
+        'name' => 'cliente.nombre', // The db column name
+        'label' => "Cliente", // Table column heading
+        'searchLogic' => function ($query, $column, $searchTerm) {
+            $query->orWhereHas('cliente', function($q) use ($searchTerm){
+                $q->where('nombre', 'like', '%'.$searchTerm.'%');
+            });
+        }
+     ]);
 
-         $this->crud->addColumn([
-            'name' => "fecha_salida", // The db column name
-            'label' => "Fecha De Salida", // Table column heading
-            'type' => "datetime",
-             // 'format' => 'l j F Y H:i:s', // use something else than the base.default_datetime_format config value
-         ]);
+    $this->crud->addColumn([
+        'name' => 'habitacion.numero', // The db column name
+        'label' => "Habitación", // Table column heading
+        'searchLogic' => function ($query, $column, $searchTerm) {
+            $query->orWhereHas('habitacion', function($q) use ($searchTerm){
+                $q->where('numero', 'like', '%'.$searchTerm.'%');
+            });
+        }
+     ]);
 
-         $this->crud->addColumn([
-            'name' => 'status_reservacion',
-            'label' => 'Status De La Reservación',
-            'type' => 'boolean',
-            'options' => [0 => 'Inactiva', 1 => 'Activa']
-        ]);
-        $this->crud->addColumn([
-            'name' => 'costo_total',
-            'label' => 'Costo Total',
-            'type' => 'text'
-        ]);
-        
-        $this->crud->addColumn([
-            'name' => 'Habitacion.numero', // The db column name
-            'label' => "Habitación", // Table column heading
-         ]);
 
-         $this->crud->addColumn([
-            'name' => 'Cliente.nombre', // The db column name
-            'label' => "Cliente", // Table column heading
-         ]);
-         $this->crud->addColumn([
-            'name' => 'MetodoPago.nombre', // The db column name
-            'label' => "Métodos De Pago", // Table column heading
-         ]);
-         $this->crud->addColumn([
-            'name' => 'Promocion.nombre', // The db column name
-            'label' => "Promociones", // Table column heading
-         ]);
+     $this->crud->addColumn([
+        'name' => "fecha_entrada", // The db column name
+        'label' => "Fecha De Entrada", // Table column heading
+        'type' => "datetime",
+         // 'format' => 'l j F Y H:i:s', // use something else than the base.default_datetime_format config value
+     ]);
 
-         $this->crud->addFilter([
-            'type'  => 'date',
-            'name'  => 'fecha_entrada',
-            'label' => 'Fecha De Entrada',
-          ],
-          false,
-          function ($value) { // if the filter is active, apply these constraints
-                $this->crud->addClause('whereDate', 'fecha_entrada', $value);
-          });
+     $this->crud->addColumn([
+        'name' => "fecha_salida", // The db column name
+        'label' => "Fecha De Salida", // Table column heading
+        'type' => "datetime",
+         // 'format' => 'l j F Y H:i:s', // use something else than the base.default_datetime_format config value
+     ]);
 
-          $this->crud->addFilter([
-            'type'  => 'date',
-            'name'  => 'fecha_salida',
-            'label' => 'Fecha De Salida',
-          ],
-          false,
-          function ($value) { // if the filter is active, apply these constraints
-                $this->crud->addClause('whereDate', 'fecha_salida', $value);
-          });
+     $this->crud->addColumn([
+        'name' => 'status_reservacion',
+        'label' => 'Status De La Reservación',
+        'type' => 'boolean',
+        'options' => [0 => 'Inactiva', 1 => 'Activa'],
+    ]);
 
-          $this->crud->addFilter([
-            'name'  => 'tipoHabitacion',
-            'type'  => 'select2',
-            'label' => 'Tipo De Habitación'
-          ], function () {
-          return [
-            1 => 'Normal',
-            2 => 'Suite',
-            3 => 'Lujo',
-          ];
-          }, function ($value) { // if the filter is active
-                $this->crud->addClause('where', 'tipo_habitacion_id', $value);
-          });
+    $this->crud->addColumn([
+        'name' => 'costo_total',
+        'label' => 'Costo Total',
+        'type' => 'text',
+        'searchLogic' => function ($query, $column, $searchTerm) {
+            $query->orWhere('costo_total', 'like', '%'.$searchTerm.'%');
+        }
+    ]);
 
-          $this->crud->addFilter([
-            'name'  => 'status',
-            'type'  => 'select2',
-            'label' => 'Status De La Reservación'
-          ], function () {
-            return [
-              1 => 'Activa',
-              2 => 'Inactiva',
-            ];
-          }, function ($value) { // if the filter is active
-                $this->crud->addClause('where', 'status_reservacion', $value);
-          });
+     $this->crud->addColumn([
+        'name' => 'metodoPago.nombre', // The db column name
+        'label' => "Métodos De Pago", // Table column heading
+        'searchLogic' => function ($query, $column, $searchTerm) {
+            $query->orWhereHas('metodoPago', function($q) use ($searchTerm){
+                $q->where('nombre', 'like', '%'.$searchTerm.'%');
+            });
+        }
+     ]);
 
-          $this->crud->addFilter([
-            'type' => 'text',
-            'name' => 'costo_total',
-            'label'=> 'Costo Total'
-          ], 
-          false, 
-          function($value) { // if the filter is active
-             $this->crud->addClause('where', 'costo_total', '=', $value);
-          });
+     $this->crud->addColumn([
+        'name' => 'promocion.nombre', // The db column name
+        'label' => "Promociones", // Table column heading
+        'searchLogic' => function ($query, $column, $searchTerm) {
+            $query->orWhereHas('promocion', function($q) use ($searchTerm){
+                $q->where('nombre', 'like', '%'.$searchTerm.'%');
+            });
+        }
+     ]);
 
-          $this->crud->addFilter([
-            'type' => 'text',
-            'name' => 'habitacion_id',
-            'label'=> 'Habitación'
-          ], 
-          false, 
-          function($value) { // if the filter is active
-             $this->crud->addClause('where', 'habitacion_id', '=', $value);
-          });
 
-          $this->crud->addFilter([
-            'name' => 'cliente_id',
-            'type' => 'select2',
-            'label'=> 'Clientes'
-          ], function() {
-              return Cliente::all()->pluck('nombre', 'id')->toArray();
-          }, function($value) { // if the filter is active
-              $this->crud->addClause('where', 'cliente_id', $value);
-          });
 
-          $this->crud->addFilter([
-            'name' => 'metodo_pago_id',
-            'type' => 'select2',
-            'label'=> 'Metodos De Pago'
-          ], function() {
-              return MetodoPago::all()->pluck('nombre', 'id')->toArray();
-          }, function($value) { // if the filter is active
-              $this->crud->addClause('where', 'metodo_pago_id', $value);
-          });
+     $this->crud->addFilter([
+        'type'  => 'date',
+        'name'  => 'fecha_entrada',
+        'label' => 'Fecha De Entrada',
+      ],
+      false,
+      function ($value) { // if the filter is active, apply these constraints
+            $this->crud->addClause('whereDate', 'fecha_entrada', $value);
+      });
 
-          $this->crud->addFilter([
-            'name' => 'promocion_id',
-            'type' => 'select2',
-            'label'=> 'Promociones'
-          ], function() {
-              return Promocion::all()->pluck('nombre', 'id')->toArray();
-          }, function($value) { // if the filter is active
-              $this->crud->addClause('where', 'promocion_id', $value);
-          });
+      $this->crud->addFilter([
+        'type'  => 'date',
+        'name'  => 'fecha_salida',
+        'label' => 'Fecha De Salida',
+      ],
+      false,
+      function ($value) { // if the filter is active, apply these constraints
+            $this->crud->addClause('whereDate', 'fecha_salida', $value);
+      });
+
+      $this->crud->addFilter([
+        'name'  => 'tipoHabitacion',
+        'type'  => 'select2',
+        'label' => 'Tipo De Habitación'
+      ], function () {
+      return [
+        1 => 'Normal',
+        2 => 'Suite',
+        3 => 'Lujo',
+      ];
+      }, function ($value) { // if the filter is active
+            $this->crud->addClause('where', 'tipo_habitacion_id', $value);
+      });
+
+      $this->crud->addFilter([
+        'name'  => 'status',
+        'type'  => 'select2',
+        'label' => 'Status De La Reservación'
+      ], function () {
+        return [
+          1 => 'Activa',
+          2 => 'Inactiva',
+        ];
+      }, function ($value) { // if the filter is active
+            $this->crud->addClause('where', 'status_reservacion', $value);
+      });
+
+      $this->crud->addFilter([
+        'type' => 'text',
+        'name' => 'costo_total',
+        'label'=> 'Costo Total'
+      ], 
+      false, 
+      function($value) { // if the filter is active
+         $this->crud->addClause('where', 'costo_total', '=', $value);
+      });
+
+      $this->crud->addFilter([
+        'type' => 'text',
+        'name' => 'habitacion_id',
+        'label'=> 'Habitación'
+      ], 
+      false, 
+      function($value) { // if the filter is active
+         $this->crud->addClause('where', 'habitacion_id', '=', $value);
+      });
+
+      $this->crud->addFilter([
+        'name' => 'cliente_id',
+        'type' => 'select2',
+        'label'=> 'Clientes'
+      ], function() {
+          return Cliente::all()->pluck('nombre', 'id')->toArray();
+      }, function($value) { // if the filter is active
+          $this->crud->addClause('where', 'cliente_id', $value);
+      });
+
+      $this->crud->addFilter([
+        'name' => 'metodo_pago_id',
+        'type' => 'select2',
+        'label'=> 'Metodos De Pago'
+      ], function() {
+          return MetodoPago::all()->pluck('nombre', 'id')->toArray();
+      }, function($value) { // if the filter is active
+          $this->crud->addClause('where', 'metodo_pago_id', $value);
+      });
+
+      $this->crud->addFilter([
+        'name' => 'promocion_id',
+        'type' => 'select2',
+        'label'=> 'Promociones'
+      ], function() {
+          return Promocion::all()->pluck('nombre', 'id')->toArray();
+      }, function($value) { // if the filter is active
+          $this->crud->addClause('where', 'promocion_id', $value);
+      });
     }
 
     protected function setupCreateOperation()
