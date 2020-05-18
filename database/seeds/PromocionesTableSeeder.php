fcb6f2a90479c7bf4e03582f3ea6a9463a0af38d
chicloket6 <samarripa_17@hotmail.com>
2020-05-18 18:16:42 -0500

    cambio seeder promociones

--- database/seeds/PromocionesTableSeeder.php
+++ database/seeds/PromocionesTableSeeder.php
@@ -13,23 +13,23 @@ class PromocionesTableSeeder extends Seeder
     public function run()
     {
         $promocion = new Promocion();
-        $promocion->nombre = '20% en suites';
-        $promocion->fecha_inicio = '2020-04-20 00:00:00';
-        $promocion->fecha_final = '2020-05-20 23:59:59';
+        $promocion->nombre = '20% por apertura';
+        $promocion->fecha_inicio = '2020-05-20 00:00:00';
+        $promocion->fecha_final = '2020-06-20 23:59:59';
         $promocion->descuento = 20;
         $promocion->save();
 
         $promocion = new Promocion();
-        $promocion->nombre = '15% en habitaciones de lujo';
-        $promocion->fecha_inicio = '2020-04-20 00:00:00';
-        $promocion->fecha_final = '2020-05-20 23:59:59';
+        $promocion->nombre = '15% para todos';
+        $promocion->fecha_inicio = '2020-05-20 00:00:00';
+        $promocion->fecha_final = '2020-06-20 23:59:59';
         $promocion->descuento = 15;
         $promocion->save();
 
         $promocion = new Promocion();
-        $promocion->nombre = '10% en habitaciones normales';
-        $promocion->fecha_inicio = '2020-04-20 00:00:00';
-        $promocion->fecha_final = '2020-05-20 23:59:59';
+        $promocion->nombre = '10% en familia';
+        $promocion->fecha_inicio = '2020-05-20 00:00:00';
+        $promocion->fecha_final = '2020-06-20 23:59:59';
         $promocion->descuento = 10;
         $promocion->save();
     }
